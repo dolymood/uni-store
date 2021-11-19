82bbb1294da98ad05b707a231324cb85e8d9770b
dolymood <dolymood@gmail.com>
2021-11-19 14:32:39 +0800

    feat: support useSetup(setup, props)

--- packages/react/src/setup.ts
+++ packages/react/src/setup.ts
@@ -1,7 +1,36 @@
 import { reactive } from '@uni-store/core'
-import { useMemo, DependencyList } from 'react'
+import type { UnwrapNestedRefs } from '@uni-store/core'
+import { useMemo, useRef } from 'react'
+import type { DependencyList } from 'react'
 
-export function useSetup<SS extends object> (setup: () => SS, deps: DependencyList = []) {
-  const setupState = useMemo(setup, deps)
+/**
+ * @deprecated Use `useSetup(setup: (props: Props) => SS, props: Props)`
+ */
+function useSetup<SS extends object, Deps extends DependencyList> (setup: () => SS, deps: Deps): UnwrapNestedRefs<SS>
+function useSetup<SS extends object> (setup: () => SS): UnwrapNestedRefs<SS>
+function useSetup<SS extends object, Props> (setup: (props: Props) => SS, props: Props): UnwrapNestedRefs<SS>
+function useSetup<SS extends object, Props, Deps> (setup: (props?: Props) => SS, props?: Props | Deps) {
+  if (Array.isArray(props)) {
+    // props is DependencyList
+    return reactive(useMemo(setup, props))
+  }
+  let propsRef = useRef<Props>()
+  let resultRef = useRef<SS>()
+  const setupState = useMemo(() => {
+    if (!resultRef.current) {
+      // first in
+      propsRef.current = reactive(props ? { ...props } : {}) as Props
+      const setupResult = setup(propsRef.current)
+      resultRef.current = setupResult
+    } else {
+      // keep same refer, just update propsRef.current
+      Object.assign(propsRef.current, props)
+    }
+    return resultRef.current!
+  }, [props])
   return reactive(setupState)
+}
+
+export {
+  useSetup
 }
