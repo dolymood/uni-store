c9c0875e155b9296e2750d32711a60c924e86d2d
dolymood <dolymood@gmail.com>
2022-03-18 15:54:14 +0800

    feat: export named api from reactivity

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,62 @@
-export * from '@vue/reactivity'
+export {
+  // core
+  reactive,
+  ref,
+  readonly,
+  // utilities
+  unref,
+  proxyRefs,
+  isRef,
+  toRef,
+  toRefs,
+  isProxy,
+  isReactive,
+  isReadonly,
+  isShallow,
+  // advanced
+  customRef,
+  triggerRef,
+  shallowRef,
+  shallowReactive,
+  shallowReadonly,
+  markRaw,
+  toRaw,
+  // effect
+  effect,
+  stop,
+  // effect scope
+  effectScope,
+  getCurrentScope,
+  onScopeDispose
+} from '@vue/reactivity'
+export type {
+  Ref,
+  ToRef,
+  ToRefs,
+  UnwrapRef,
+  ShallowRef,
+  ShallowUnwrapRef,
+  CustomRefFactory,
+  ReactiveFlags,
+  DeepReadonly,
+  ShallowReactive,
+  UnwrapNestedRefs,
+  ComputedRef,
+  WritableComputedRef,
+  WritableComputedOptions,
+  ComputedGetter,
+  ComputedSetter,
+  ReactiveEffectRunner,
+  ReactiveEffectOptions,
+  ReactiveEffect,
+  EffectScope,
+  EffectScheduler,
+  DebuggerOptions,
+  DebuggerEvent,
+  DebuggerEventExtraInfo,
+  TrackOpTypes,
+  TriggerOpTypes
+} from '@vue/reactivity'
 export {
   nextTick,
   watch,
